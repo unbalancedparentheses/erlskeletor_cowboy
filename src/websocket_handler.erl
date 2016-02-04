68a6d2e16c7d76c9720141aa960e2268f2ccfe73
manuelolmos <manuolmos88@gmail.com>
2016-02-04 00:02:06 -0300

    Send the time every second via websocket

--- src/websocket_handler.erl
+++ src/websocket_handler.erl
@@ -7,6 +7,7 @@
 	]).
 
 init(Req, _Opts) ->
+    erlang:start_timer(1000, self(), get_time()),
     {cowboy_websocket, Req, #{}}.
 
 websocket_handle(Frame = {text, _}, Req, State) ->
@@ -16,5 +17,12 @@ websocket_handle(_Frame, Req, State) ->
 
 websocket_info({log, Text}, Req, State) ->
     {reply, {text, Text}, Req, State};
+websocket_info({timeout, _Ref, Msg}, Req, State) ->
+    erlang:start_timer(1000, self(), get_time()),
+    {reply, {text, Msg}, Req, State};
 websocket_info(_Info, Req, State) ->
     {ok, Req, State}.
+
+get_time() ->
+    {{_Year, _Month, _Day}, {Hour, Min, Sec}} = erlang:localtime(),
+    lists:concat([Hour, ":", Min, ":", Sec]).
